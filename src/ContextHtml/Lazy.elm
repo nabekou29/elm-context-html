bc09fbc37463bae66bb6be74b1c4a7d286c5321f
Kohei Watanabe <kohei_watanabe@fringe81.com>
2021-10-10 07:52:42 +0900

    lazyが機能しない問題を修正

--- src/ContextHtml/Lazy.elm
+++ src/ContextHtml/Lazy.elm
@@ -1,8 +1,8 @@
-module ContextHtml.Lazy exposing (lazy, lazy2, lazy3, lazy4, lazy5, lazy6, lazy7, lazy8)
+module ContextHtml.Lazy exposing (lazy, lazy2, lazy3, lazy4, lazy5, lazy6, lazy0)
 
-{-| Wrapped [Html.lazy](https://package.elm-lang.org/packages/elm/html/1.0.0/Html-Lazy) module for ContextHtml.
+{-| Wrapped [Html.Lazy](https://package.elm-lang.org/packages/elm/html/1.0.0/Html-Lazy) module for ContextHtml.
 
-@docs lazy, lazy2, lazy3, lazy4, lazy5, lazy6, lazy7, lazy8
+@docs lazy, lazy2, lazy3, lazy4, lazy5, lazy6, lazy0
 
 -}
 
@@ -10,58 +10,114 @@ import ContextHtml exposing (ContextHtml, applyContext, withContext)
 import Html.Lazy as Lazy
 
 
-{-| -}
-lazy : (a -> ContextHtml ctx msg) -> a -> ContextHtml ctx msg
+{-| Checks one argument and context.
+-}
+lazy :
+    (a -> ContextHtml ctx msg)
+    -> a
+    -> ContextHtml ctx msg
 lazy html a =
     withContext <|
         \ctx ->
-            Lazy.lazy (\a_ -> applyContext ctx (html a_)) a
+            Lazy.lazy3 applyContextForLazy ctx html a
 
 
-{-| -}
-lazy2 : (a -> b -> ContextHtml ctx msg) -> a -> b -> ContextHtml ctx msg
+{-| Checks two arguments and context.
+-}
+lazy2 :
+    (a -> b -> ContextHtml ctx msg)
+    -> a
+    -> b
+    -> ContextHtml ctx msg
 lazy2 html a b =
     withContext <|
-        \ctx -> Lazy.lazy2 (\a_ b_ -> applyContext ctx (html a_ b_)) a b
+        \ctx -> Lazy.lazy4 applyContextForLazy2 ctx html a b
 
 
 {-| -}
-lazy3 : (a -> b -> c -> ContextHtml ctx msg) -> a -> b -> c -> ContextHtml ctx msg
+lazy3 :
+    (a -> b -> c -> ContextHtml ctx msg)
+    -> a
+    -> b
+    -> c
+    -> ContextHtml ctx msg
 lazy3 html a b c =
     withContext <|
-        \ctx -> Lazy.lazy3 (\a_ b_ c_ -> applyContext ctx (html a_ b_ c_)) a b c
+        \ctx -> Lazy.lazy5 applyContextForLazy3 ctx html a b c
 
 
 {-| -}
-lazy4 : (a -> b -> c -> d -> ContextHtml ctx msg) -> a -> b -> c -> d -> ContextHtml ctx msg
+lazy4 :
+    (a -> b -> c -> d -> ContextHtml ctx msg)
+    -> a
+    -> b
+    -> c
+    -> d
+    -> ContextHtml ctx msg
 lazy4 html a b c d =
     withContext <|
-        \ctx -> Lazy.lazy4 (\a_ b_ c_ d_ -> applyContext ctx (html a_ b_ c_ d_)) a b c d
+        \ctx -> Lazy.lazy6 applyContextForLazy4 ctx html a b c d
 
 
 {-| -}
-lazy5 : (a -> b -> c -> d -> e -> ContextHtml ctx msg) -> a -> b -> c -> d -> e -> ContextHtml ctx msg
+lazy5 :
+    (a -> b -> c -> d -> e -> ContextHtml ctx msg)
+    -> a
+    -> b
+    -> c
+    -> d
+    -> e
+    -> ContextHtml ctx msg
 lazy5 html a b c d e =
     withContext <|
-        \ctx -> Lazy.lazy5 (\a_ b_ c_ d_ e_ -> applyContext ctx (html a_ b_ c_ d_ e_)) a b c d e
+        \ctx -> Lazy.lazy7 applyContextForLazy5 ctx html a b c d e
 
 
 {-| -}
-lazy6 : (a -> b -> c -> d -> e -> f -> ContextHtml ctx msg) -> a -> b -> c -> d -> e -> f -> ContextHtml ctx msg
+lazy6 :
+    (a -> b -> c -> d -> e -> f -> ContextHtml ctx msg)
+    -> a
+    -> b
+    -> c
+    -> d
+    -> e
+    -> f
+    -> ContextHtml ctx msg
 lazy6 html a b c d e f =
     withContext <|
-        \ctx -> Lazy.lazy6 (\a_ b_ c_ d_ e_ f_ -> applyContext ctx (html a_ b_ c_ d_ e_ f_)) a b c d e f
+        \ctx -> Lazy.lazy8 applyContextForLazy6 ctx html a b c d e f
 
 
-{-| -}
-lazy7 : (a -> b -> c -> d -> e -> f -> g -> ContextHtml ctx msg) -> a -> b -> c -> d -> e -> f -> g -> ContextHtml ctx msg
-lazy7 html a b c d e f g =
+{-| Checks only context.
+-}
+lazy0 :
+    ContextHtml ctx msg
+    -> ContextHtml ctx msg
+lazy0 html =
     withContext <|
-        \ctx -> Lazy.lazy7 (\a_ b_ c_ d_ e_ f_ g_ -> applyContext ctx (html a_ b_ c_ d_ e_ f_ g_)) a b c d e f g
+        \ctx ->
+            Lazy.lazy2 applyContext ctx html
 
 
-{-| -}
-lazy8 : (a -> b -> c -> d -> e -> f -> g -> h -> ContextHtml ctx msg) -> a -> b -> c -> d -> e -> f -> g -> h -> ContextHtml ctx msg
-lazy8 html a b c d e f g h =
-    withContext <|
-        \ctx -> Lazy.lazy8 (\a_ b_ c_ d_ e_ f_ g_ h_ -> applyContext ctx (html a_ b_ c_ d_ e_ f_ g_ h_)) a b c d e f g h
+applyContextForLazy ctx html a =
+    applyContext ctx (html a)
+
+
+applyContextForLazy2 ctx html a b =
+    applyContext ctx (html a b)
+
+
+applyContextForLazy3 ctx html a b c =
+    applyContext ctx (html a b c)
+
+
+applyContextForLazy4 ctx html a b c d =
+    applyContext ctx (html a b c d)
+
+
+applyContextForLazy5 ctx html a b c d e =
+    applyContext ctx (html a b c d e)
+
+
+applyContextForLazy6 ctx html a b c d e f =
+    applyContext ctx (html a b c d e f)
